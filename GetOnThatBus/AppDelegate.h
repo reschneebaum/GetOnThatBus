2c10ab1592db6d5dd308560d2aaa2dd9c559c681
Rachel Schneebaum <reschneebaum@gmail.com>
2015-07-28 14:19:29 -0500

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Rachel Schneebaum on 7/28/15.
+//  Copyright (c) 2015 Rachel Schneebaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
